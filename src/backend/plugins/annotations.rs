0d04dceb4ec33e634146937b6ac79ecc2ab7d601
Liam Boreback <boreback.work@gmail.com>
2025-11-17 17:43:42 +0100

    Make the annotations be python accessible

--- src/backend/plugins/annotations.rs
+++ src/backend/plugins/annotations.rs
@@ -1,25 +1,28 @@
-use pyo3::prelude::*;
-use serde::{Deserialize, Serialize};
+use pyo3::{prelude::*};
 
-#[derive(Serialize, Deserialize, Debug)]
-struct FunctionAnnotation {
+#[pyclass]
+#[derive(Debug, Clone)]
+enum PrimitiveType {
+    Int,
+    Float,
+    Str,
+    Bool,
+}
+
+#[pyclass]
+#[derive(Debug, Clone)]
+enum TypeInfo {
+    Union(Vec<TypeInfo>),
+    List(Vec<TypeInfo>),
+    Dict(Vec<TypeInfo>, Vec<TypeInfo>),
+    Primitive(PrimitiveType),
+    Unknown(String),
+}
+
+#[pyclass]
+#[derive(Debug, Clone)]
+struct FunctionInfo {
     name: String,
-    args: Vec<(String, Annotation)>,
-    // return_type: Annotation,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-enum Annotation {
-    SimpleType { type_name: String },
-    Union { type_name: String, args: Vec<Annotation> },
-    List { type_name: String, args: Vec<Annotation> },
-    Dict { type_name: String, args: Vec<Annotation>, values: Vec<Annotation> },
-}
-
-#[pyfunction]
-fn load_annotations() -> PyResult<Vec<FunctionAnnotation>> {
-    let json_data = fs::read_to_string("annotations.json")?;
-    let annotations: Vec<FunctionAnnotation> = serde_json::from_str(&json_data)?;
-    Ok(annotations)
+    args: Vec<(String, TypeInfo)>,
+    return_type: TypeInfo,
 }
